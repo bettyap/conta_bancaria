87b61adc63694eeb2e4b20209a5f7ddf318f7196
Elisabeth Aparecida <beth_ap@live.com>
2023-11-16 14:16:13 -0300

    feat: criado parte visual e add lógica de sair

--- src/conta_bancaria/util/Cores.java
+++ src/conta_bancaria/util/Cores.java
@@ -1,5 +1,10 @@
 package conta_bancaria.util;
 
-class Cores {
+public class Cores {
 
+	public static final String TEXT_PURPLE_BRIGHT = "\033[0;95m"; 
+	public static final String TEXT_CYAN_BRIGHT = "\033[0;96m";   
+	public static final String TEXT_WHITE_BRIGHT = "\033[0;97m";  
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
 }
